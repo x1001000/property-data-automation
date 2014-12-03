e10d19de73ade793b2333e57f42fc12220d96ac9
十百千 <x1001000@users.noreply.github.com>
2014-12-03 13:46:27 +0800

    Update rename-with-code.py

--- rename-with-code.py
+++ rename-with-code.py
@@ -21,3 +21,5 @@ with open('list.txt', 'r') as f:
                         except:
                             print '\trenamed ' + match.group(1) + '_no_exif' + '.jpg'
                             call('cp ' + folder + '/' + file + ' ' + folder + '/' + match.group(1) + '_no_exif' + '.jpg', shell=True)
+call('rm -f list.txt', shell=True)
+call('rm -f code.txt', shell=True)
