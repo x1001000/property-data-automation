38811b680dfdc88d0e0d37ef7b68ca1216e94498
十百千 <x1001000@users.noreply.github.com>
2015-03-17 20:40:47 +0800

    4 cases
    time =
    if match: break
    if not renamed:

--- rename.py
+++ rename.py
@@ -6,19 +6,26 @@ call('ls ' + folder + ' > list.txt', shell=True)
 with open('list.txt', 'r') as f:
     files = f.read().split()
     for file in files:
-        print 'Scanning ' + file
+        renamed = False
+        print 'rename ' + file
+        with open(folder + '/' + file, 'r') as f:
+            try:
+                tags = process_file(f)
+                time = str(tags['EXIF DateTimeOriginal']).replace(' ', '-').replace(':', '')
+            except:
+                time = 'NO-EXIF'
+
         call('zbarimg -q ' + folder + '/' + file + ' > code.txt', shell=True)
         with open('code.txt', 'r') as f:
             for line in f:
                 match = search(':([0-9]{12})', line)
                 if match:
-                    with open(folder + '/' + file, 'r') as f:
-                        try:
-                            tags = process_file(f)
-                            time = str(tags['EXIF DateTimeOriginal']).replace(' ', '-').replace(':', '')
-                            print '\trenamed ' + match.group(1) + '_' + time + '.jpg'
-                            call('cp ' + folder + '/' + file + ' ' + folder + '/' + match.group(1) + '_' + time + '.jpg', shell=True)
-                        except:
-                            print '\trenamed ' + match.group(1) + '_no_exif' + '.jpg'
-                            call('cp ' + folder + '/' + file + ' ' + folder + '/' + match.group(1) + '_no_exif' + '.jpg', shell=True)
+                    print '\t\t\t\tto ' + match.group(1) + '_' + time + '.jpg'
+                    call('mv ' + folder + '/' + file + ' ' + folder + '/' + match.group(1) + '_' + time + '.jpg', shell=True)
+                    renamed = True
+                    break
+
+        if not renamed:
+            print '\t\t\t\tto ' + file[:-4] + '_' + time + '.jpg'
+            call('mv ' + folder + '/' + file + ' ' + folder + '/' + file[:-4] + '_' + time + '.jpg', shell=True)
 call('rm -f list.txt code.txt', shell=True)
